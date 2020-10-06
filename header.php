7a6dc1a3df7a2a3f3c61cf565284dfc45a102f49
Alivia Salsabila <alivia_salsabila24@apps.ipb.ac.id>
2020-10-06 20:51:36 +0700

    Tampil data di Pertemuan 2

---
+++ header.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <style>
+            #badan{
+                background-color: #578a99;
+            }            
+            #navi{
+                margin-left: 7%;
+                height: max-content;
+                width: 10%;
+                border-radius: 30px;
+                padding: 2% 0;
+                font-size: 1.1em;
+                background-color: #e3ebed;
+            }
+            .kotak{
+                background-color: #e3ebed;
+                min-height: 35%;
+                max-width: 60%;
+                border-radius: 30px;
+                margin: 3% auto;
+            }
+            .isi{
+                padding-top: 1%;             
+            }
+            /* input[type=text], select{
+                width: 60%;
+                border-radius: 10px;
+                margin-left: 5%;
+                height: 13%;
+            } */
+            .baris, #apa{
+                padding-top: 0.5%;
+                padding-top: 3%;
+           }
+           input[type=checkbox], input[type=radio]{
+                margin-left: 5%;
+           }
+        </style>
+    </head>
+    <body id="badan">
+        <div class="w3-sidebar  w3-bar-block" id="navi">
+            <a href="../Pertemuan 1" class="w3-bar-item w3-button" style="padding-top: 10%;text-align: center;">Pertemuan 1</a>
+            <a href="" class="w3-bar-item w3-button" style="text-align: center;">Pertemuan 2</a>
+            <a href="../Pertemuan 3/home.php" class="w3-bar-item w3-button" style="text-align: center;">Pertemuan 3</a>
+        </div>
+        <div class="kotak">
+           
+            <div class="isi" style="text-align: center">
---
+++ tampil_data.php
@@ -0,0 +1,16 @@
+<?php include 'header.php' ?>
+<a href="insert.php" >tambah data</a>
+<h2>Daftar data dalam database</h2>
+<?php
+    include "koneksi.php";
+    $r = mysqli_query($kon,"SELECT * FROM mahasiswa");
+    $i = 0;
+    while($brs = mysqli_fetch_assoc($r)){
+        echo ++$i.". ".$brs['nama'];
+        
+        //tambahan ada di sini untuk edit, br pindahin ke bawah ya anak2 
+        echo ' <a href="delete.php?id='.$brs['id'].'">delete</a><br>';
+    }
+
+?>
+<?php include 'footer.php' ?>
